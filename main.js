e7b02ebb01998e2a0ef50bc2cc7b5b5ec3488007
Fass2017SSK! <severine.kombo@epitech.eu>
2024-11-02 22:38:13 +0100

    puissance4

--- main.js
+++ main.js
@@ -9,16 +9,14 @@ var colonnesactu;
 var rows = 6;
 var colonnes = 7;
 
-
-
 window.onload = function () {
   setJeu();
 };
 
-
 function setJeu() {
   jeu = [];
   colonnesactu = [5, 5, 5, 5, 5, 5, 5];
+  perdu = false; 
 
   for (let a = 0; a < rows; a++) {
     let row = [];
@@ -46,22 +44,7 @@ function setPiece() {
 
   a = colonnesactu[b];
   if (a < 0) {
-    return;
-  }
-
-  // vérifier si la colonne est pleine
-  let colonnePleine = true;
-  for (let i = 0; i < rows; i++) {
-    if (jeu[i][b] === " ") {
-      colonnePleine = false;
-      break;
-    }
-  }
-  
-  if (colonnePleine) {
-    alert("Game Over");
-    perdu = true;
-    return;
+    return; 
   }
 
   jeu[a][b] = joueuractu;
@@ -74,107 +57,72 @@ function setPiece() {
     joueuractu = joueurRed;
   }
 
-  a -= 1;
-  colonnesactu[b] = a;
+  colonnesactu[b] = a - 1;
 
   checkWinner();
 }
 
 function checkWinner() {
-  // gagner à l'horizontal
   for (let a = 0; a < rows; a++) {
     for (let b = 0; b < colonnes - 3; b++) {
-      if (jeu[a][b] != " ") {
-        if (
-          jeu[a][b] == jeu[a][b + 1] &&
-          jeu[a][b + 1] == jeu[a][b + 2] &&
-          jeu[a][b + 2] == jeu[a][b + 3]
-        ) {
-          setWinner(a, b);
-          return;
-        }
+      if (jeu[a][b] != " " &&
+        jeu[a][b] == jeu[a][b + 1] &&
+        jeu[a][b + 1] == jeu[a][b + 2] &&
+        jeu[a][b + 2] == jeu[a][b + 3]) {
+        setWinner(a, b);
+        return;
       }
     }
   }
 
-  //gagner à la verticale
   for (let b = 0; b < colonnes; b++) {
     for (let a = 0; a < rows - 3; a++) {
-      if (jeu[a][b] != " ") {
-        if (
-          jeu[a][b] == jeu[a + 1][b] &&
-          jeu[a + 1][b] == jeu[a + 2][b] &&
-          jeu[a + 2][b] == jeu[a + 3][b]
-        ) {
-          setWinner(a, b);
-          return;
-        }
+      if (jeu[a][b] != " " &&
+        jeu[a][b] == jeu[a + 1][b] &&
+        jeu[a + 1][b] == jeu[a + 2][b] &&
+        jeu[a + 2][b] == jeu[a + 3][b]) {
+        setWinner(a, b);
+        return;
       }
     }
   }
 
-  //gagner à la diagonal gauche
   for (let a = 0; a < rows - 3; a++) {
     for (let b = 0; b < colonnes - 3; b++) {
-      if (jeu[a][b] != " ") {
-        if (
-          jeu[a][b] == jeu[a + 1][b + 1] &&
-          jeu[a + 1][b + 1] == jeu[a + 2][b + 2] &&
-          jeu[a + 2][b + 2] == jeu[a + 3][b + 3]
-        ) {
-          setWinner(a, b);
-          return;
-        }
+      if (jeu[a][b] != " " &&
+        jeu[a][b] == jeu[a + 1][b + 1] &&
+        jeu[a + 1][b + 1] == jeu[a + 2][b + 2] &&
+        jeu[a + 2][b + 2] == jeu[a + 3][b + 3]) {
+        setWinner(a, b);
+        return;
       }
     }
   }
 
-  //gagner à la diagonal droite
   for (let a = 3; a < rows; a++) {
     for (let b = 0; b < colonnes - 3; b++) {
-      if (jeu[a][b] != " ") {
-        if (
-          jeu[a][b] == jeu[a - 1][b + 1] &&
-          jeu[a - 1][b + 1] == jeu[a - 2][b + 2] &&
-          jeu[a - 2][b + 2] == jeu[a - 3][b + 3]
-        ) {
-          setWinner(a, b);
-          return;
-        }
+      if (jeu[a][b] != " " &&
+        jeu[a][b] == jeu[a - 1][b + 1] &&
+        jeu[a - 1][b + 1] == jeu[a - 2][b + 2] &&
+        jeu[a - 2][b + 2] == jeu[a - 3][b + 3]) {
+        setWinner(a, b);
+        return;
       }
     }
   }
 }
-
 
 function setWinner(a, b) {
   let winner = document.getElementById("winner");
   if (jeu[a][b] == joueurRed) {
     winner.innerText = "Red WINNER";
-    A++;
-    pointRed.innerHTML = A;
-    localStorage.setItem(A);
-    
   } else {
     winner.innerText = "Yellow WINNER";
-    B++;
-    pointYellow.innerHTML = B;
-    localStorage.setItem(A);
   }
-  perdu = true;
+  perdu = true; 
 }
 
-
-
 const resetBtn = document.getElementById("reset");
-
-resetBtn.addEventListener("click", function() {
-  window.location.reload();
+resetBtn.addEventListener("click", function () {
+  window.location.reload(); 
 });
-
-restartBtn.addEventListener("click", function() {
-  window.location.href = 'index.html'; 
-});
-
-let joueurRedPoints = 0;
-let joueurYellowPoints = 0;joueurRedPoints++;
